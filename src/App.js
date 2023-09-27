f24011fcd1c5123b2a145932166c1947d26e6fd4
Tinhuynh211 <tinhuynh211@gmail.com>
2023-09-27 16:51:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+
+export default App;
