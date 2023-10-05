dfc359d6c72b86574b118ba3e8a681caeab6661f
Tinhuynh211 <tinhuynh211@gmail.com>
2023-10-05 20:39:14 +0700

    29

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import { Component } from 'react';
 import Main from './components/MainComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <div className='text-center'>
+          <Main />
+          </div>
+        
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,7 +7,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   function RenderMenuItem ({dish, onClick}) {
     return (
         <Card
-            onClick={() => onClick(dish.id)}>
+           >
             <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
