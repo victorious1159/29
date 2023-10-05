0dae1ee553670007df8f6de2a9ce99c4d6f7399a
Tinhuynh211 <tinhuynh211@gmail.com>
2023-10-05 19:56:58 +0700

    28

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Hello from './Hello';
-import Counter from './Counter';
-import Counter1 from './Counter1';
-import HelloAsClass from './HelloAsClass';
-import HelloAsFunction from './HelloAsFunction';
-import Menu from './components/MenuComponent';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
